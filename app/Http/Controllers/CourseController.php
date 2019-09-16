91ecf9f74c39388ade9886061c1e7a1072bd9899
Deyvison Rocha <deyvison@gmail.com>
2019-09-16 18:13:03 -0300

    Módulo de curso

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Course;
+use Illuminate\Http\Request;
+use App\Http\Requests\Courses\CourseStoreRequest;
+use App\Http\Requests\Courses\CourseUpdateRequest;
+use App\Http\Resources\CourseResource;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $courses = Course::get()->toArray();
+
+        // return $this->responseOk($courses, 'Lista de cursos');
+        return (new CourseResource($courses))
+            ->response()
+            ->setStatusCode(200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  CourseStoreRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CourseStoreRequest $request)
+    {
+        $course = Course::create($request->all());
+
+        return new CourseResource($course);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Course  $course
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Course $course)
+    {
+        return new CourseResource($course);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  CourseUpdateRequest  $request
+     * @param  \App\Course  $course
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Course $course)
+    {
+        $course->update($request->all());
+
+        return new CourseResource($course);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Course  $course
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Course $course)
+    {
+        // Validar quando estiver com alunos relacionados quando tiver matriculas
+        
+        return $course->delete();
+    }
+}
